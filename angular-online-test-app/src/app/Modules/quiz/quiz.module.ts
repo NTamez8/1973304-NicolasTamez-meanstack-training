b9fbe1abdeefd0bf38610b0eba9a9cb725d6025a
nicolas <nicolas.tamez@icloud.com>
2021-03-24 07:29:38 -0500

    Added files for project

--- angular-online-test-app/src/app/Modules/quiz/quiz.module.ts
+++ angular-online-test-app/src/app/Modules/quiz/quiz.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { QuizDisplayComponent } from '../../Components/quiz-display/quiz-display.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [QuizDisplayComponent],
   imports: [
     CommonModule
-  ]
+  ],
+  exports:[QuizDisplayComponent]
+  
 })
 export class QuizModule { }
